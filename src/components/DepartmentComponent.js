175f2ee734deed8eac4ddde5c9d33ddcb039cee0
NgocTan12345k <kiemtienonlinee1@gmail.com>
2022-08-22 00:37:35 +0700

    Them bao cao san pham mau bang wireframe va word

--- src/components/DepartmentComponent.js
+++ src/components/DepartmentComponent.js
@@ -3,33 +3,34 @@ import { Card, CardTitle, CardBody, CardText } from "reactstrap";
 
 //Presentational Component
 class RenderDept extends Component {
-    render() {
-        return (
-            <Card>
-                <CardTitle className="m-2">{this.props.dept.name}</CardTitle>
-                <CardBody>
-                    <CardText>Số lượng nhân viên: {this.props.dept.numberOfStaff}</CardText>
-                </CardBody>
-            </Card>
-        );
-    }
+  render() {
+    return (
+      <Card>
+        <CardTitle className="m-2">{this.props.dept.name}</CardTitle>
+        <CardBody>
+          <CardText>
+            Số lượng nhân viên: {this.props.dept.numberOfStaff}
+          </CardText>
+        </CardBody>
+      </Card>
+    );
+  }
 }
 
 //Container Component
 function Department(props) {
-        const departments = props.dept.map((department) => {
-            return (
-                <div className="col-12 col-md-6 col-lg-4 mt-2 mb-2" key={department.id}>
-                    <RenderDept dept={department} />
-                </div>
-            );
-        });
-        return (
-            <div className="container">
-                <div className="row shadow m-3">{departments}</div>
-            </div>
-        );
-    }
-
+  const departments = props.dept.map((department) => {
+    return (
+      <div className="col-12 col-md-6 col-lg-4 mt-2 mb-2" key={department.id}>
+        <RenderDept dept={department} />
+      </div>
+    );
+  });
+  return (
+    <div className="container">
+      <div className="row shadow m-3">{departments}</div>
+    </div>
+  );
+}
 
 export default Department;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,32 +8,46 @@ import { STAFFS, DEPARTMENTS } from "../shared/staffs";
 import Department from "./DepartmentComponent";
 import Salary from "./SalaryComponent";
 
-    
-
 function Main() {
-    const [nhanvien] = useState({
-        staffs: STAFFS,
-        departments: DEPARTMENTS
-    });
-   
-    const StaffWithId = ({ match }) => {
-        return (
-            <StaffDetail nv = {nhanvien.staffs.filter((item) => item.id === parseInt(match.params.nhanvienId,10))[0]} />
-        );
-    }
+  const [nhanvien] = useState({
+    staffs: STAFFS,
+    departments: DEPARTMENTS,
+  });
 
+  const StaffWithId = ({ match }) => {
     return (
-        <div>
-            <Header />
-                <Switch>
-                    <Route exact path="/nhanvien" component={() => <StaffList staffs={nhanvien.staffs} />} />
-                    <Route path="/nhanvien/:nhanvienId" component={StaffWithId} />
-                    <Route path="/phongban" component={() => <Department dept={nhanvien.departments} />} />
-                    <Route path="/bangluong" component={() => <Salary luong={nhanvien.staffs} />} />
-                </Switch>
-            <Footer />
-        </div>
+      <StaffDetail
+        nv={
+          nhanvien.staffs.filter(
+            (item) => item.id === parseInt(match.params.nhanvienId, 10)
+          )[0]
+        }
+      />
     );
+  };
+
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route
+          exact
+          path="/nhanvien"
+          component={() => <StaffList staffs={nhanvien.staffs} />}
+        />
+        <Route path="/nhanvien/:nhanvienId" component={StaffWithId} />
+        <Route
+          path="/phongban"
+          component={() => <Department dept={nhanvien.departments} />}
+        />
+        <Route
+          path="/bangluong"
+          component={() => <Salary luong={nhanvien.staffs} />}
+        />
+      </Switch>
+      <Footer />
+    </div>
+  );
 }
 
 export default Main;
